6184b7bd09f85ec1071bed36759fcb577242962d
git-shashwat <shashwat824@gmail.com>
2020-01-15 19:34:21 +0530

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: 'production',
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public/dist'),
         filename: 'bundle.js'
     },
     module: {
